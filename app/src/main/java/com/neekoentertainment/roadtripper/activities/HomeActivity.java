179cf1b72c0c211eb223e65d3180dddc97457fc7
Nicolas Girard <nicolasg37@gmail.com>
2016-04-05 18:21:41 -0700

    merge navigation drawer to master

--- app/src/main/java/com/neekoentertainment/roadtripper/activities/HomeActivity.java
+++ app/src/main/java/com/neekoentertainment/roadtripper/activities/HomeActivity.java
@@ -4,9 +4,9 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -62,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-        mapView = (MapView) findViewById(R.id.mapview);
+        mMapView = (MapView) findViewById(R.id.mapview);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
             @Override
